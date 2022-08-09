2cebec6706a4b58881251aeac6f12e8591beae1f
Archi00 <albert.archi@gmail.com>
2022-08-09 21:09:38 +0200

    update

--- views/js/cookiesbanner.js
+++ views/js/cookiesbanner.js
@@ -1,8 +1,3 @@
-$(document).ready(function () {
-    function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-        var expires = "expires=" + d.toUTCString();
-        document.cookie = cname + "=" + cvalue + "; " + expires;
-    }
+$(() => {
+    console.log("Test");
 });
